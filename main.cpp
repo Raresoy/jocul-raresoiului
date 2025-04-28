d29770e2444650706f803f49e37d0ea9a227ccd9
Raresoy <nutizic123@gmail.com>
2025-04-28 17:08:54 +0300

    powerups

--- main.cpp
+++ main.cpp
@@ -7,21 +7,21 @@
 
 enum TipInamic { RANDOM, CHASER, SNIPER };
 
-class Harta{ //reprezinta un reper cartezian xoy pe care il vom folosi pentru a misca entitatile pe harta
+class Pozitie{ //reprezinta un reper cartezian xoy pe care il vom folosi pentru a misca entitatile pe harta
 private:
     float x, y;
 public:
-    Harta(float x = 0, float y = 0) : x(x), y(y) {}
+    Pozitie(float x = 0, float y = 0) : x(x), y(y) {}
     float getX() const { return x; }
     float getY() const { return y; }
     void miscari(float dx, float dy, float maxX = 100, float maxY = 100) {//misca o entitate fara ca aceasta sa iasa din bounds
         x = std::max(0.0f, std::min(x + dx, maxX));
         y = std::max(0.0f, std::min(y + dy, maxY));
     }
-    float distanta(const Harta& other) const {//calculeaza distanta dintre doua entitati
+    float distanta(const Pozitie& other) const {//calculeaza distanta dintre doua entitati
         return std::sqrt((x - other.x)*(x - other.x) + (y - other.y)*(y - other.y));
     }
-    friend std::ostream& operator<<(std::ostream& os, const Harta& v) {
+    friend std::ostream& operator<<(std::ostream& os, const Pozitie& v) {
         os << "(" << v.x << ", " << v.y << ")";
         return os;
     }
@@ -30,7 +30,7 @@ public:
 
 class Jucator{
 private:
-    Harta pozitie;
+    Pozitie pozitie;
     int viata;
     int cooldown;//timpul in care jucatorul e invulnerabil  
     int scor;
@@ -38,6 +38,8 @@ private:
     int experienta;
     int reloadTime;
     const int reloadMax = 3;
+    int rapidFire = 0;
+    int multiShot = 0;
     std::vector<std::string> inventar;
 public:
     Jucator(float x, float y, int viata = 5) : pozitie(x, y), viata(viata), cooldown(0), scor(0), nivel(1), experienta(0), reloadTime(0) {}
@@ -47,6 +49,8 @@ public:
     void tick() {
         if (cooldown > 0) cooldown--;//scade durata de invulnerabilitate
         if (reloadTime > 0) reloadTime--;//reload
+        if (rapidFire > 0) rapidFire--;
+        if (multiShot > 0) multiShot--;
     }
     void lovit(int damage = 1) {
         if (cooldown == 0 && viata > 0) {
@@ -66,7 +70,7 @@ public:
     {
         if(reloadTime == 0)
         {
-            reloadTime = reloadMax;
+            reloadTime = (rapidFire > 0 ? reloadMax / 2 : reloadMax);
             return true;
         }
         return false;
@@ -77,7 +81,9 @@ public:
     void adaugaItem(const std::string& item) {//doua iteme de viata si invulnerabilitate
         inventar.push_back(item);
         if (item == "Viata") viata++;
-        else if (item == "Scut") cooldown += 2;
+        else if (item == "Scut") cooldown += 3;
+        else if (item == "RapidFire") rapidFire = 10;
+        else if (item == "MultiShot") multiShot = 10;
     }
     void dodge() {
         cooldown = 2;
@@ -90,7 +96,7 @@ public:
         scor += s; 
         castigaExperienta(s);
     }
-    const Harta& getPozitie() const {
+    const Pozitie& getPozitie() const {
         return pozitie;
     }
     friend std::ostream& operator<<(std::ostream& os, const Jucator& j) {
@@ -120,11 +126,31 @@ public:
     
 };
     
-    
+class Powerup {
+private:
+    Pozitie pozitie;
+    std::string tip;//"Viata", "RapidFire", "Scut", "MultiShot"
+public:
+    Powerup(float x, float y, const std::string& tip) : pozitie(x, y), tip(tip) {}
+    const Pozitie& getPozitie() const { return pozitie; }
+    const std::string& getTip() const { return tip; }
+    bool verificaColectare(Jucator& jucator) {
+        if (pozitie.distanta(jucator.getPozitie()) < 2.0f) {
+            jucator.adaugaItem(tip);
+            return true;
+        }
+        return false;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Powerup& p) {
+        os << "Powerup [" << p.tip << "] la " << p.pozitie;
+        return os;
+    }
+};
+     
 
 class Proiectil{
 private:
-    Harta pozitie;
+    Pozitie pozitie;
     float dx, dy;
     bool activ;
     std::string tip;
@@ -160,12 +186,12 @@ public:
     
 class Inamic{
 private:
-    Harta pozitie;
+    Pozitie pozitie;
     int cooldown;
     TipInamic tip;//vor exista mai multe tipuri de inamici ca sa nu se plictiseasca jucatorul luptandu-se cu un singur fel de inamic
 public://random se misca random, chaser fuge dupa jucator, iar sniper sta pe loc si trage lovituri puternice si precise
     Inamic(float x, float y, TipInamic tip = RANDOM) : pozitie(x, y), cooldown(0), tip(tip) {}
-    void actualizeaza(const Harta& jucatorPoz) {
+    void actualizeaza(const Pozitie& jucatorPoz) {
         float dx = 0, dy = 0;
         if (tip == RANDOM) {
             dx = ((rand() % 3) - 1) * 0.5f;
@@ -181,7 +207,7 @@ public://random se misca random, chaser fuge dupa jucator, iar sniper sta pe loc
         else if (tip == SNIPER) {}
         pozitie.miscari(dx, dy);
         if (cooldown > 0) cooldown--;
-        }
+    }
     bool poateTrage() {
         if (cooldown == 0) {
             cooldown = (tip == SNIPER ? 2 : 4);
@@ -189,7 +215,7 @@ public://random se misca random, chaser fuge dupa jucator, iar sniper sta pe loc
         }
         return false;
     }
-    Proiectil trageLaJucator(const Harta& tinta) {//inamicii trag doar spre jucator
+    Proiectil trageLaJucator(const Pozitie& tinta) {//inamicii trag doar spre jucator
         float dx = tinta.getX() - pozitie.getX();
         float dy = tinta.getY() - pozitie.getY();
         float dist = std::sqrt(dx * dx + dy * dy);
@@ -201,6 +227,7 @@ public://random se misca random, chaser fuge dupa jucator, iar sniper sta pe loc
         return os;
     }
 };
+
             
 int main() {
     srand(static_cast<unsigned int>(time(0)));
@@ -255,6 +282,3 @@ int main() {
     std::cout << "\n=== Runda terminata ===\n";
     return 0;
 }
-
-
-
