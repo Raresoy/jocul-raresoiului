1251be7ab296d3921a9c58bc2106153e0870e64f
Raresoy <nutizic123@gmail.com>
2025-05-07 14:22:07 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -533,7 +533,7 @@ int main() {
     run.ruleaza();
     ResourceManager& res = ResourceManager::Instance();
     sf::Texture& backgroundTexture = res.getTexture("airplane.png");
-    sf::Font& font = res.getFont("Arial");
+    sf::Font& font = res.getFont("fonts/FiraSans-Regular");
     sf::Text text(font, "Hello world!", 24);
     sf::Sprite background(backgroundTexture); 
     return 0;
